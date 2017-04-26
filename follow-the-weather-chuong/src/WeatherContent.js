59a040c682cfb2efb27adea6f1c7656f21f8cc94
X00123743 <X00123743@computing.stu.it-tallaght.ie>
2017-04-26 17:57:41 +0100

    update not fully worked

--- follow-the-weather-chuong/src/WeatherContent.js
+++ follow-the-weather-chuong/src/WeatherContent.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
-import WeatherData from './WeatherData';
+import weatherData from './WeatherData';
 
 class WeatherContent extends Component {
+  renderTableHeader() {
+    return (
+      <tr>
+        <th>Current</th>
+        <th>Today</th>
+        <th>Tonight</th>
+        <th>Tomorrow</th>
+      </tr>
+    )
+  }
+
+  renderTableBody(currentData) {
+    return (
+      <tbody>
+        <tr>
+          <td>{currentData.current.temperature}</td>
+        </tr>
+      </tbody>
+    )
+  }
+
   render() {
     const {placeName} = this.props;
+    const currentData = weatherData[placeName];
+
+    if (currentData) {
+      return null;
+    }
 
     return (
-      <div className="wheather-content">
+      <div className="weather-content">
         <h2>Weather in {placeName}</h2>
+
+      <table>
+        <thead>
+          {this.renderTableHeader()}
+        </thead>
+          {this.renderTableBody(currentData)}
+      </table>
       </div>
-    );
+    )
   }
 }
 
